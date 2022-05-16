77028d239f22e7988f477ece8bc3002ea02e7742
mhmdpra <mhmdpra17@gmail.com>
2022-05-16 22:42:21 +0800

    percobaan 3

---
+++ app/src/main/java/com/example/F55119006/FragmentAccount.java
@@ -0,0 +1,22 @@
+package com.example.F55119006;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FragmentAccount extends Fragment {
+    View view;
+    public FragmentAccount() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.account_fragment, container,false);
+        return view;
+    }
+}
